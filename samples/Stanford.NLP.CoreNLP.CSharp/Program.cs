b2dbb3aeea60a7090db32fa5540da0ab7d217ca9
volhav <volha.varabei@gmail.com>
2015-10-11 23:16:31 +0300

    c# samples added as projects

---
+++ samples/Stanford.NLP.CoreNLP.CSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using java.util;
+using java.io;
+using edu.stanford.nlp.pipeline;
+using Console = System.Console;
+
+namespace Stanford.NLP.CoreNLP.CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Path to the folder with models extracted from `stanford-parser-3.5.2-models.jar`
+            var jarRoot = @"..\..\..\..\paket-files\nlp.stanford.edu\stanford-corenlp-full-2015-04-20\models";
+
+            // Text for processing
+            var text = "Kosgi Santosh sent an email to Stanford University. He didn't get a reply.";
+
+            // Annotation pipeline configuration
+            var props = new Properties();
+            props.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse");//ner fails -> dcoref depends on ner
+            props.setProperty("sutime.binders", "0");
+
+            // We should change current directory, so StanfordCoreNLP could find all the model files automatically
+            var curDir = Environment.CurrentDirectory;
+            Directory.SetCurrentDirectory(jarRoot);
+            var pipeline = new StanfordCoreNLP(props);
+            Directory.SetCurrentDirectory(curDir);
+
+            // Annotation
+            var annotation = new Annotation(text);
+            pipeline.annotate(annotation);
+
+            // Result - Pretty Print
+            using (var stream = new ByteArrayOutputStream())
+            {
+                pipeline.prettyPrint(annotation, new PrintWriter(stream));
+                Console.WriteLine(stream.toString());
+                stream.close();
+            }
+        }
+    }
+}
---
+++ samples/Stanford.NLP.NER.CSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using edu.stanford.nlp.ie.crf;
+using Console = System.Console;
+
+namespace Stanford.NLP.NER.CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Path to the folder with classifies models
+            var jarRoot = @"..\..\..\..\paket-files\nlp.stanford.edu\stanford-ner-2015-04-20";
+            var classifiersDirecrory = jarRoot + @"\classifiers";
+
+            // Loading 3 class classifier model
+            var classifier =CRFClassifier.getClassifierNoExceptions(
+                classifiersDirecrory + @"\english.all.3class.distsim.crf.ser.gz");
+            
+            var s1 = "Good afternoon Rajat Raina, how are you today?";
+            Console.WriteLine("{0}\n", classifier.classifyToString(s1));
+
+            var s2 = "I go to school at Stanford University, which is located in California.";
+            Console.WriteLine("{0}\n", classifier.classifyWithInlineXML(s2));
+
+            Console.WriteLine("{0}\n", classifier.classifyToString(s2, "xml", true));
+        }
+    }
+}
