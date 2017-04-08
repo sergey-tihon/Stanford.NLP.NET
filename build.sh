d61f8395e85f792f6ca8400ba29ca5aed80522ac
Sergey Tihon <sergey.tihon@gmail.com>
2017-04-08 15:01:17 +0300

    Added data restore on mono

--- build.sh
+++ build.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-if test "$OS" = "Windows_NT"
-then
-  # use .Net
 
-  .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
+cd data
 
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
-else
-  # use mono
-
-  mono .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+mono ../.paket/paket.exe restore
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  exit $exit_code
 fi
+
+cd ..
+
+mono .paket/paket.exe restore
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  exit $exit_code
+fi
+
+mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+
